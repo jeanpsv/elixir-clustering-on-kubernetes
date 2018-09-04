94ad36d4ba931346e4cacffa5e216d3e51299f39
Jean Vasconcelos <jean.vasconcelos6@gmail.com>
2018-09-04 16:03:51 -0300

    use peerage configuration for kubernetes

--- elixir/config/prod.exs
+++ elixir/config/prod.exs
@@ -5,4 +5,11 @@ config :elixir_clustering_on_kubernetes, :env, :prod
 config :logger,
   level: :warn
 
-config :peerage, via: Peerage.Via.Udp, serves: true
+# docker-compose.yaml configuration
+# config :peerage, via: Peerage.Via.Udp, serves: true
+
+# kubernetes configuration
+config :peerage, via: Peerage.Via.Dns,
+  dns_name: "myapp-headless-service.${NAMESPACE}.svc.cluster.local",
+  app_name: "myapp",
+  interval: 5
